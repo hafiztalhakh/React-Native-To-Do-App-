e28d5bc1dd754f0dcb48fdb61e2c46fd11552fb6
Talha Khalid <38666963+Talha619@users.noreply.github.com>
2020-07-29 20:25:20 +0500

    To Do App

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [courseGoals, setCourseGoals] = useState('');
+
+    // const courseGolasHandler = 
+
+    const consoleDikhao = () => {
+        console.log(courseGoals);
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Course Goals"
+                    onChangeText={e => { setCourseGoals(e) }}
+                    value={courseGoals}
+                />
+                <Button title="Add" style={styles.btn} onPress={consoleDikhao} />
+            </View>
+            <View />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        padding: 50,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        width: '80%',
+        padding: 5,
+        paddingLeft: 10,
+        borderWidth: 1,
+        borderRadius: 5,
+    },
+    btn: {
+        height: 50,
+    }
 });
